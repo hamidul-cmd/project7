e77d5ee09299ca4065cf2976d5da0e3597bfa7d7
Hamidul Islam Maruf <hamidulislammaruf2@gmail.com>
2025-02-24 23:12:07 +0800

    alll done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,260 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
+import Revolution from "../Components/Revolution";
 
 function Contact() {
   return (
-    <div className='text-red-600'>
-      tis is contact
-    </div>
-  )
+    <>
+      <section className="px-4 xll:px-20 3xl:px-36.6 grid grid-cols-1 border-b border-dark-15 overflow-hidden xll:grid-cols-4 xll:gap-14.5 3xl:gap-24.5">
+        <div className="py-5 relative before:content[''] before:absolute before:h-[1px] before:w-full before:bg-dark-15 before:bottom-0 xll:before:w-[1px] xll:before:h-full xll:before:right-[-30px] xll:py-14.5 3xl:before:right-[-50px] 3xl:py-20">
+          <h3 className="text-base font-medium leading-130 tracking-tight text-white mb-5 xll:text-lg xll:leading-130 xll:mb-7.5 3xl:text-xl 3xl:leading-130 3xl:mb-12.5">
+            General Inquiries
+          </h3>
+          <div className="pl-12.5 xll:pl-0">
+            <div className="flex items-end px-5 py-3.5 gap-1 bg-dark-10 border border-dark-15 w-fit rounded-md mb-2.5 3xl:py-4.8 3xl:px-6 3xl:rounded-lg 3xl:mb-[15px]">
+              <span className="text-sm leading-150 tracking-tight text-gray-60 3xl:text-lg 3xl:leading-150">
+                contact@ai-podcasts.com
+              </span>
+              <span>
+                <svg
+                  className="h-5 w-5 3xl:h-6 3xl:w-6"
+                  viewBox="0 0 18 19"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M6.1875 3.3125L14.625 3.3125C14.7742 3.3125 14.9173 3.37176 15.0227 3.47725C15.1282 3.58274 15.1875 3.72582 15.1875 3.875V12.3125C15.1875 12.6232 14.9357 12.875 14.625 12.875C14.3143 12.875 14.0625 12.6232 14.0625 12.3125V5.233L3.77275 15.5227C3.55308 15.7424 3.19692 15.7424 2.97725 15.5227C2.75758 15.3031 2.75758 14.9469 2.97725 14.7273L13.267 4.4375L6.1875 4.4375C5.87684 4.4375 5.625 4.18566 5.625 3.875C5.625 3.56434 5.87684 3.3125 6.1875 3.3125Z"
+                    fill="#FFD11A"
+                  />
+                </svg>
+              </span>
+            </div>
+            <div className="flex items-end px-5 py-3.5 gap-1 bg-dark-10 border border-dark-15 w-fit rounded-md 3xl:py-4.8 3xl:px-6 3xl:rounded-lg">
+              <span className="text-sm leading-150 tracking-tight text-gray-60 3xl:text-lg 3xl:leading-150">
+                +1 (123) 456-7890
+              </span>
+              <span>
+                <svg
+                  className="h-5 w-5 3xl:h-6 3xl:w-6"
+                  viewBox="0 0 18 19"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M6.1875 3.3125L14.625 3.3125C14.7742 3.3125 14.9173 3.37176 15.0227 3.47725C15.1282 3.58274 15.1875 3.72582 15.1875 3.875V12.3125C15.1875 12.6232 14.9357 12.875 14.625 12.875C14.3143 12.875 14.0625 12.6232 14.0625 12.3125V5.233L3.77275 15.5227C3.55308 15.7424 3.19692 15.7424 2.97725 15.5227C2.75758 15.3031 2.75758 14.9469 2.97725 14.7273L13.267 4.4375L6.1875 4.4375C5.87684 4.4375 5.625 4.18566 5.625 3.875C5.625 3.56434 5.87684 3.3125 6.1875 3.3125Z"
+                    fill="#FFD11A"
+                  />
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="py-5 relative before:content[''] before:absolute before:h-[1px] before:w-full before:bg-dark-15 before:bottom-0 xll:before:w-[1px] xll:before:h-full xll:before:right-[-30px] xll:py-14.5 3xl:before:right-[-50px] 3xl:py-20">
+          <h3 className="text-base font-medium leading-130 tracking-tight text-white mb-5 xll:text-lg xll:leading-130 xll:mb-7.5 3xl:text-xl 3xl:leading-130 3xl:mb-12.5">
+            Technical Support
+          </h3>
+          <div className="pl-12.5 xll:pl-0">
+            <div className="flex items-end px-5 py-3.5 gap-1 bg-dark-10 border border-dark-15 w-fit rounded-md mb-2.5 3xl:py-4.8 3xl:px-6 3xl:rounded-lg 3xl:mb-[15px]">
+              <span className="text-sm leading-150 tracking-tight text-gray-60 3xl:text-lg 3xl:leading-150">
+                contact@ai-podcasts.com
+              </span>
+              <span>
+                <svg
+                  className="h-5 w-5 3xl:h-6 3xl:w-6"
+                  viewBox="0 0 18 19"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M6.1875 3.3125L14.625 3.3125C14.7742 3.3125 14.9173 3.37176 15.0227 3.47725C15.1282 3.58274 15.1875 3.72582 15.1875 3.875V12.3125C15.1875 12.6232 14.9357 12.875 14.625 12.875C14.3143 12.875 14.0625 12.6232 14.0625 12.3125V5.233L3.77275 15.5227C3.55308 15.7424 3.19692 15.7424 2.97725 15.5227C2.75758 15.3031 2.75758 14.9469 2.97725 14.7273L13.267 4.4375L6.1875 4.4375C5.87684 4.4375 5.625 4.18566 5.625 3.875C5.625 3.56434 5.87684 3.3125 6.1875 3.3125Z"
+                    fill="#FFD11A"
+                  />
+                </svg>
+              </span>
+            </div>
+            <div className="flex items-end px-5 py-3.5 gap-1 bg-dark-10 border border-dark-15 w-fit rounded-md 3xl:py-4.8 3xl:px-6 3xl:rounded-lg">
+              <span className="text-sm leading-150 tracking-tight text-gray-60 3xl:text-lg 3xl:leading-150">
+                +1 (123) 456-7890
+              </span>
+              <span>
+                <svg
+                  className="h-5 w-5 3xl:h-6 3xl:w-6"
+                  viewBox="0 0 18 19"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M6.1875 3.3125L14.625 3.3125C14.7742 3.3125 14.9173 3.37176 15.0227 3.47725C15.1282 3.58274 15.1875 3.72582 15.1875 3.875V12.3125C15.1875 12.6232 14.9357 12.875 14.625 12.875C14.3143 12.875 14.0625 12.6232 14.0625 12.3125V5.233L3.77275 15.5227C3.55308 15.7424 3.19692 15.7424 2.97725 15.5227C2.75758 15.3031 2.75758 14.9469 2.97725 14.7273L13.267 4.4375L6.1875 4.4375C5.87684 4.4375 5.625 4.18566 5.625 3.875C5.625 3.56434 5.87684 3.3125 6.1875 3.3125Z"
+                    fill="#FFD11A"
+                  />
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="py-5 relative before:content[''] before:absolute before:h-[1px] before:w-full before:bg-dark-15 before:bottom-0 xll:before:w-[1px] xll:before:h-full xll:before:right-[-30px] xll:py-14.5 3xl:before:right-[-50px] 3xl:py-20">
+          <h3 className="text-base font-medium leading-130 tracking-tight text-white mb-5 xll:text-lg xll:leading-130 xll:mb-7.5 3xl:text-xl 3xl:leading-130 3xl:mb-12.5">
+            Our Office
+          </h3>
+          <div className="pl-12.5 xll:pl-0">
+            <p className="text-base leading-150 tracking-tight text-gray-60 mb-3 3xl:text-lg 3xl:leading-150">
+              Address: 123 AI Tech Avenue, Techville, 54321
+            </p>
+            <Link className="flex items-end px-5 py-3.5 gap-1 bg-dark-10 border border-dark-15 w-fit rounded-md mb-2.5 3xl:py-4.8 3xl:px-6 3xl:rounded-lg">
+              <span className="text-sm leading-150 tracking-tight text-gray-60 3xl:text-lg 3xl:leading-150">
+                Get Directions
+              </span>
+              <span>
+                <svg
+                  className="h-5 w-5 3xl:h-6 3xl:w-6"
+                  viewBox="0 0 18 19"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M6.1875 3.3125L14.625 3.3125C14.7742 3.3125 14.9173 3.37176 15.0227 3.47725C15.1282 3.58274 15.1875 3.72582 15.1875 3.875V12.3125C15.1875 12.6232 14.9357 12.875 14.625 12.875C14.3143 12.875 14.0625 12.6232 14.0625 12.3125V5.233L3.77275 15.5227C3.55308 15.7424 3.19692 15.7424 2.97725 15.5227C2.75758 15.3031 2.75758 14.9469 2.97725 14.7273L13.267 4.4375L6.1875 4.4375C5.87684 4.4375 5.625 4.18566 5.625 3.875C5.625 3.56434 5.87684 3.3125 6.1875 3.3125Z"
+                    fill="#FFD11A"
+                  />
+                </svg>
+              </span>
+            </Link>
+          </div>
+        </div>
+        <div className="py-5 xll:py-14.5 3xl:py-20">
+          <h3 className="text-base leading-130 tracking-tight text-white mb-5 xll:text-lg xll:leading-130 xll:mb-7.5 3xl:text-xl 3xl:leading-130 3xl:mb-12.5">
+            Connect with Us
+          </h3>
+          <div className="pl-12.5 flex gap-2.5 xll:pl-0">
+            <Link className="py-3.5 px-6 bg-dark-10 border border-dark-15 rounded-md grow flex items-center justify-center">
+              <div>
+                <svg
+                  className="h-5 w-5 3xl:h-6 3xl:w-6"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.5475 5.98396C17.5552 6.15996 17.5552 6.32796 17.5552 6.50396C17.563 11.84 13.6426 18 6.47319 18C4.35865 18 2.2827 17.368 0.5 16.184C0.808692 16.224 1.11738 16.24 1.42608 16.24C3.1779 16.24 4.88343 15.632 6.26483 14.504C4.59789 14.472 3.1316 13.344 2.62226 11.696C3.20877 11.816 3.81072 11.792 4.3818 11.624C2.56824 11.256 1.26401 9.59997 1.2563 7.67197C1.2563 7.65597 1.2563 7.63997 1.2563 7.62397C1.79651 7.93597 2.40617 8.11197 3.02356 8.12797C1.31803 6.94396 0.78554 4.58396 1.81966 2.73595C3.80301 5.26396 6.72015 6.79196 9.85337 6.95996C9.53696 5.55996 9.96913 4.08796 10.9801 3.09595C12.5467 1.56795 15.0162 1.64795 16.498 3.27195C17.37 3.09595 18.2112 2.75995 18.9752 2.28795C18.682 3.22395 18.0723 4.01596 17.262 4.51996C18.0337 4.42396 18.79 4.20796 19.5 3.88795C18.9752 4.70396 18.3115 5.40796 17.5475 5.98396Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+            </Link>
+            <Link className="py-3.5 px-6 bg-dark-10 border border-dark-15 rounded-md grow flex items-center justify-center">
+              <div>
+                <svg
+                  className="h-5 w-5 3xl:h-6 3xl:w-6"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11.2812 10.1897C11.2812 13.332 8.75578 15.8793 5.6407 15.8793C2.52561 15.8793 0 13.3314 0 10.1897C0 7.04806 2.52542 4.5 5.6407 4.5C8.75597 4.5 11.2812 7.04749 11.2812 10.1897Z"
+                    fill="white"
+                  />
+                  <path
+                    d="M17.4688 10.1897C17.4688 13.1475 16.2061 15.5462 14.6485 15.5462C13.0908 15.5462 11.8281 13.1475 11.8281 10.1897C11.8281 7.23198 13.0906 4.83328 14.6483 4.83328C16.2059 4.83328 17.4686 7.23121 17.4686 10.1897"
+                    fill="white"
+                  />
+                  <path
+                    d="M19.9993 10.1898C19.9993 12.8392 19.5553 14.9883 19.0074 14.9883C18.4595 14.9883 18.0156 12.8398 18.0156 10.1898C18.0156 7.53974 18.4597 5.39121 19.0074 5.39121C19.5551 5.39121 19.9993 7.53955 19.9993 10.1898Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+            </Link>
+            <Link className="py-3.5 px-6 bg-dark-10 border border-dark-15 rounded-md grow flex items-center justify-center">
+              <div>
+                <svg
+                  className="h-5 w-5 3xl:h-6 3xl:w-6"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.3858 1.33334H2.61157C2.27221 1.33334 1.94676 1.46814 1.7068 1.7081C1.46684 1.94806 1.33203 2.27352 1.33203 2.61287V17.3871C1.33203 17.7265 1.46684 18.0519 1.7068 18.2919C1.94676 18.5319 2.27221 18.6667 2.61157 18.6667H17.3858C17.7252 18.6667 18.0506 18.5319 18.2906 18.2919C18.5306 18.0519 18.6654 17.7265 18.6654 17.3871V2.61287C18.6654 2.27352 18.5306 1.94806 18.2906 1.7081C18.0506 1.46814 17.7252 1.33334 17.3858 1.33334ZM6.49833 16.0992H3.89231V7.8213H6.49833V16.0992ZM5.19351 6.67417C4.89791 6.6725 4.60941 6.58331 4.36445 6.41784C4.11949 6.25238 3.92903 6.01805 3.81711 5.74444C3.70519 5.47084 3.67683 5.17021 3.73561 4.8805C3.79439 4.59079 3.93767 4.32498 4.14737 4.11662C4.35706 3.90826 4.62379 3.7667 4.91387 3.70978C5.20395 3.65286 5.50439 3.68315 5.77727 3.79683C6.05016 3.9105 6.28325 4.10246 6.44714 4.34848C6.61104 4.5945 6.69838 4.88356 6.69814 5.17917C6.70093 5.37708 6.66385 5.57353 6.5891 5.75681C6.51435 5.94008 6.40346 6.10643 6.26305 6.24594C6.12265 6.38545 5.95559 6.49526 5.77184 6.56883C5.58809 6.64241 5.3914 6.67823 5.19351 6.67417ZM16.1039 16.1064H13.4991V11.5841C13.4991 10.2504 12.9321 9.83871 12.2003 9.83871C11.4275 9.83871 10.6692 10.4213 10.6692 11.6178V16.1064H8.06314V7.82732H10.5693V8.97445H10.603C10.8545 8.46528 11.7356 7.595 13.0802 7.595C14.5343 7.595 16.1051 8.45806 16.1051 10.9858L16.1039 16.1064Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className="px-4 xll:px-20 3xl:px-36.6 xll:flex xll:gap-14.5 xll:items-center border-b border-dark-15">
+        <div className="py-10 border-b border-dark-15 xll:border-b-0 xll:w-[412px] 3xl:w-[519px]">
+          <div className="w-fit mb-5 xll:mb-7.5 3xl:mb-12.5">
+            <svg
+              className="h-12.5 w-12.5 xll:h-14.5 xll:w-14.5 3xl:h-20 3xl:w-20"
+              viewBox="0 0 50 50"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M25 15C25 20.5228 20.5228 25 15 25C9.47715 25 5 20.5228 5 15C5 9.47715 9.47715 5 15 5C20.5228 5 25 9.47715 25 15Z"
+                fill="#404040"
+              />
+              <path
+                d="M45 15C45 20.5228 40.5228 25 35 25C29.4772 25 25 20.5228 25 15C25 9.47715 29.4772 5 35 5C40.5228 5 45 9.47715 45 15Z"
+                fill="#FFD11A"
+              />
+              <path
+                d="M45 35C45 40.5228 40.5228 45 35 45C29.4772 45 25 40.5228 25 35C25 29.4772 29.4772 25 35 25C40.5228 25 45 29.4772 45 35Z"
+                fill="#404040"
+              />
+              <path
+                d="M25 35C25 40.5228 20.5228 45 15 45C9.47715 45 5 40.5228 5 35C5 29.4772 9.47715 25 15 25C20.5228 25 25 29.4772 25 35Z"
+                fill="#FFD11A"
+              />
+            </svg>
+          </div>
+          <h2 className="text-[28px] leading-130 tracking-tight font-medium text-white xll:text-[44px] 3xl:text-[58px]">
+            Get in Touch with AI Podcasts
+          </h2>
+        </div>
+        <form className="grid grid-cols-1 gap-5 px-5 py-10 xll:w-[808px] xll:p-14.5 xll:pr-0 xll:grid-cols-2 xll:gap-x-12.5 xll:gap-y-7.5 xll:border-l border-dark-15 3xl:w-[997px] 3xl:p-20 3xl:pr-0 3xl:gap-12.5">
+          <div>
+            <h3 className="text-base leading-150 tracking-tight font-medium text-white mb-3">First Name</h3>
+            <input type="text" placeholder="Enter First Name" className="block bg-dark-10 p-4 border border-dark-15 rounded-md outline-none w-full placeholder:text-sm placeholder:leading-150 placeholder:text-dark-40 text-sm leading-150 text-dark-40" />
+          </div>
+          <div>
+            <h3 className="text-base leading-150 tracking-tight font-medium text-white mb-3">Last Name</h3>
+            <input type="text" placeholder="Enter Last Name" className="block bg-dark-10 p-4 border border-dark-15 rounded-md outline-none w-full placeholder:text-sm placeholder:leading-150 placeholder:text-dark-40 text-sm leading-150 text-dark-40" />
+          </div>
+          <div>
+            <h3 className="text-base leading-150 tracking-tight font-medium text-white mb-3">Email</h3>
+            <input type="email" placeholder="Enter your Email" className="block bg-dark-10 p-4 border border-dark-15 rounded-md outline-none w-full placeholder:text-sm placeholder:leading-150 placeholder:text-dark-40 text-sm leading-150 text-dark-40" />
+          </div>
+          <div>
+            <h3 className="text-base leading-150 tracking-tight font-medium text-white mb-3">Phone Number</h3>
+            <input type="number" name="phone" id="phone" placeholder="Enter Phone Number" className="block bg-dark-10 p-4 border border-dark-15 rounded-md outline-none w-full placeholder:text-sm placeholder:leading-150 placeholder:text-dark-40 text-sm leading-150 text-dark-40" />
+          </div>
+          <div className="xll:col-span-2">
+            <h3 className="text-base leading-150 tracking-tight font-medium text-white mb-3">Message</h3>
+            <textarea name="massage" id="massage" placeholder="Enter your Message" className="w-full bg-dark-10 p-4 border border-dark-15 rounded-md placeholder:text-sm placeholder:leading-150 placeholder:text-dark-40 text-sm leading-150 text-dark-40 h-[127px] outline-none resize-none xll:h-[163px]"></textarea>
+          </div>
+          <div className="xll:col-span-2 xll:flex xll:justify-between xll:items-center">
+            <div className="flex gap-1.5 items-center mb-5">
+              <input type="checkbox" name="" id="" />
+              <p className="text-sm leading-150 text-white">I agree with Terms of Use and Privacy Policy</p>
+            </div>
+            <button type="submit" className="block py-3.5 px-7.5 rounded-lg bg-yellow-55 text-sm font-medium leading-6 text-dark-8 w-full xll:w-fit">Send</button>
+          </div>
+        </form>
+      </section>
+      <Revolution/>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
